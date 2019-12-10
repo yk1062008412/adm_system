e19d1eb5c6e2aa3bc0fc53267ea52dc0ebb4b6ef
yk1062008412 <yk1062008412@gmail.com>
2019-12-10 14:12:24 +0800

    format

--- vue.config.js
+++ vue.config.js
@@ -2,26 +2,26 @@
 
 const path = require('path');
 function resolve(dir) {
-    return path.join(__dirname, dir);
+  return path.join(__dirname, dir);
 }
 
 module.exports = {
-    publicPath: './',
-    devServer: {
-        open: true,
-        overlay: {
-            warnings: true,
-            errors: true
-        },
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3001/',
-                changeOrigin: true,
-            }
-        }
+  // publicPath: './',
+  devServer: {
+    open: true,
+    overlay: {
+      warnings: true,
+      errors: true
     },
-    chainWebpack: config => {
-        config.resolve.alias
-            .set('@', resolve('src'));
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001/',
+        changeOrigin: true,
+      }
     }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'));
+  }
 }
