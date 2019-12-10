ab61bf63a675694adf095c4a4579dbb9c38f43fe
liujiyu <412806117@qq.com>
2019-12-10 14:01:06 +0800

    回滚冲突

--- vue.config.js
+++ vue.config.js
@@ -6,22 +6,8 @@ function resolve(dir) {
 }
 
 module.exports = {
-    publicPath: './',
-    devServer: {
-        open: true,
-        overlay: {
-            warnings: true,
-            errors: true
-        },
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3001/',
-                changeOrigin: true,
-            }
-        }
-    },
-    chainWebpack: config => {
-        config.resolve.alias
-            .set('@', resolve('src'));
-    }
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'));
+  }
 }
